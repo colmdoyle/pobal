381746ec84ce71e5f731f125afc4a723d04c5536
Colm Doyle <colm@kitmanlabs.com>
2015-10-30 12:03:38 +0000

    Person admin improvements

--- app/admin/person.rb
+++ app/admin/person.rb
@@ -14,17 +14,29 @@ ActiveAdmin.register Person do
   permit_params :first_name, :last_name, :phone_number, :email, :website, :facebook, :twitter, :postal_address, :gender, :avatar
 
   form do |f|
-    f.input :avatar, as: :file, hint: f.template.image_tag(f.object.avatar.url(:thumb))
+    f.inputs do
+      f.input :avatar, as: :file
+      f.input :first_name
+      f.input :last_name
+      f.input :phone_number, as: :phone
+      f.input :email
+      f.input :website, as: :url
+      f.input :facebook
+      f.input :twitter
+      f.input :postal_address
+      f.input :gender
+      f.input :positions
+    end
     # Will preview the image when the object is edited
     f.actions
   end
 
-  show do |ad|
-    attributes_table do
-      row :avatar do
-        image_tag(ad.avatar.url(:thumb))
-      end
-      # Will display the image on show object page
-    end
-  end
+  # show do |ad|
+  #   attributes_table do
+  #     row :avatar do
+  #       image_tag(ad.avatar.url(:thumb))
+  #     end
+  #     # Will display the image on show object page
+  #   end
+  # end
 end
