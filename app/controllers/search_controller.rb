30fa91e0c738c3dd5f9c7ddd51d1d0a9b1c9a872
Colm Doyle <colm@kitmanlabs.com>
2015-11-01 21:59:43 +0000

    more layout stuff

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,11 +1,14 @@
 class SearchController < ApplicationController
   def index
     @location = Geocoder.coordinates(params['form-input'])
-    @constituencies = []
+    @current_reps = []
     @mapit_response = JSON.parse(HTTParty.get("#{Rails.application.secrets.mapit_url}#{@location[1]},#{@location[0]}").body)
-    @mapit_response.each do |key, value|
+    @mapit_response.each do |_, value|
       constituency = Constituency.find_by(MapItID: value['id'], const_type: value['type'])
-      @constituencies << constituency unless constituency.nil?
+      unless constituency.nil?
+        @position = Position.find_by(constituency: constituency)
+        @current_reps << @position unless @position.nil?
+      end
     end
   end
 end
