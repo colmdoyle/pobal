6a5fe5063f22e2aadd2f9bb92521d809717604f0
Colm Doyle <colm@kitmanlabs.com>
2015-11-02 09:39:03 +0000

    Group reps by body

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,15 +1,17 @@
 class SearchController < ApplicationController
   def index
     @location = Geocoder.coordinates(params['form-input'])
-    @search_results = []
+    @search_results = {}
     @mapit_response = JSON.parse(HTTParty.get("#{Rails.application.secrets.mapit_url}#{@location[1]},#{@location[0]}").body)
     @mapit_response.each do |_, value|
       constituency = Constituency.find_by(MapItID: value['id'], const_type: value['type'])
       unless constituency.nil?
+        constituency_reps = []
         @representatives = Position.where(constituency: constituency)
         for @representative in @representatives
-          @search_results << @representative unless @representative.nil?
+          constituency_reps << @representative unless @representative.nil?
         end
+        @search_results[constituency.const_type] = constituency_reps
       end
     end
   end
