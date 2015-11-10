c3fb402b8456d45f38168505612cc6e05a0dbab5
Colm Doyle <colm@kitmanlabs.com>
2015-11-10 18:20:20 +0000

    remove redundant through

--- app/models/position.rb
+++ app/models/position.rb
@@ -1,7 +1,7 @@
 class Position < ActiveRecord::Base
   belongs_to :body
   belongs_to :constituency
-  has_many :people, through: :memberships
+  has_many :people
   belongs_to :position_type
 
 end
