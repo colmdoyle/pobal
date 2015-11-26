083440a5500bdbee3971066f788f42d75412f731
Colm Doyle <colm@kitmanlabs.com>
2015-11-26 18:07:24 +0000

    Improved error pages
    closes #7

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get "/404" => "errors#not_found"
+  get "/500" => "errors#internal_server_error"
+
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
 
