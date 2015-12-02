918e1ef12a9d1beee552b7d6efdde884b69d943b
Colm Doyle <colm@kitmanlabs.com>
2015-12-02 09:18:58 +0000

    OG / SEO / Titles etc

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  def title(text)
+    content_for :title, text
+  end
+
+  def meta_tag(tag, text)
+    content_for :"meta_#{tag}", text
+  end
+
+  def yield_meta_tag(tag, default_text='')
+    content_for?(:"meta_#{tag}") ? content_for(:"meta_#{tag}") : default_text
+  end
+
 end
