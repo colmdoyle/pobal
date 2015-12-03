f4f2524cc722baa326f6d4392c8e252dd0150052
Colm Doyle <colm@kitmanlabs.com>
2015-12-03 09:49:10 +0000

    update tests

--- spec/controllers/body_controller_spec.rb
+++ spec/controllers/body_controller_spec.rb
@@ -1,18 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe BodyController, type: :controller do
-  describe "GET index" do
-    it "renders the index template" do
-      get :index
-      expect(response).to render_template("index")
-    end
-    it "has a 200 status code" do
-      get :index
-      expect(response.status).to eq(200)
-    end
-
-  end
-
   describe "GET show" do
     it "renders the show template" do
       body = FactoryGirl.create(:body)
--- spec/controllers/group_controller_spec.rb
+++ spec/controllers/group_controller_spec.rb
@@ -1,10 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe GroupController, type: :controller do
-  describe "GET index" do
-    it "renders the index template" do
-      get :index
-      expect(response).to render_template("index")
+  describe "GET show" do
+    it "renders the show template" do
+      group = FactoryGirl.create(:group)
+      get :show, :id => group.id
+      expect(response).to render_template("show")
     end
   end
 
