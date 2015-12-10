a393aa84b8d4dead705f096c8133cb3e54fb3d23
Colm Doyle <colm@kitmanlabs.com>
2015-12-10 10:27:38 +0000

    add logentries config

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,5 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+Rails.logger = Le.new(Rails.application.secrets.logentries_key, :debug => true, :local => true)
