918e5760e9c31a31cf0a80d93c56bef5158c5aca
Colm Doyle <colm@kitmanlabs.com>
2015-12-14 14:44:01 +0000

    Improve Search Controller

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,31 +1,37 @@
 class SearchController < ApplicationController
   def index
+    unless params['form-input']
+      redirect_to root_path
+    end
+
     @mapit_response = convert_address_to_mapit(params['form-input'])
     @search_results = {}
-    @mapit_response.each do |_, value|
+    if @mapit_response
+      @mapit_response.each do |_, value|
 
-      constituency_type = ConstituencyType.find_by(mapit_code: value['type'])
-      constituency = Constituency.find_by(map_it_id: value['id'], constituency_type: constituency_type)
+        constituency_type = ConstituencyType.find_by(mapit_code: value['type'])
+        constituency = Constituency.find_by(map_it_id: value['id'], constituency_type: constituency_type)
 
-      unless constituency.nil?
-        constituency_reps = []
-        @representatives = Position.where(constituency: constituency, end_date: nil).includes(:person)
+        unless constituency.nil?
+          constituency_reps = []
+          for @representative in Position.where(constituency: constituency, end_date: nil).includes(:person)
+            constituency_reps << @representative unless @representative.nil?
+          end
 
-        for @representative in @representatives
-          constituency_reps << @representative unless @representative.nil?
+          @search_results[constituency.id] = {
+            "constituency_type" => constituency_type,
+            "constituency" => constituency,
+            "representatives" => constituency_reps
+          }
         end
-
-        @search_results[constituency.id] = {
-          "constituency_type" => constituency_type,
-          "constituency" => constituency,
-          "representatives" => constituency_reps
-        }
       end
     end
   end
 
   def convert_address_to_mapit(address)
     @location = Geocoder.coordinates(address)
-    @mapit_response = JSON.parse(HTTParty.get("#{Rails.application.secrets.mapit_url}#{@location[1]},#{@location[0]}").body)
+    unless @location.nil?
+      @mapit_response = JSON.parse(HTTParty.get("#{Rails.application.secrets.mapit_url}#{@location[1]},#{@location[0]}").body)
+    end
   end
 end
