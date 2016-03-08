fba5842c09369e4837fcf0d6e099f394b9dfb35e
Colm Doyle <colm@kitmanlabs.com>
2016-03-08 09:02:15 +0000

    Load s3 assets over HTTPS

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,6 +84,7 @@ Rails.application.configure do
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
       :s3_region => ENV['AWS_S3_REGION']
-    }
+    },
+    :s3_protocol => 'https'
   }
 end
