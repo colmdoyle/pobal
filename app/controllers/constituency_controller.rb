4340d5d54d47974025c3c3e938cd259a4eba77e6
Colm Doyle <colm@kitmanlabs.com>
2016-04-26 22:08:05 +0100

    Stop listing former people connected to a constituency

--- app/controllers/constituency_controller.rb
+++ app/controllers/constituency_controller.rb
@@ -6,7 +6,5 @@ class ConstituencyController < ApplicationController
   def show
     @constituency = Constituency.find(params[:id])
     @current_members = Position.where(constituency: @constituency, end_date: nil).includes(:person).order("people.last_name ASC")
-    @former_members = Position.where(constituency: @constituency).where.not(end_date: nil).includes(:person).order("people.last_name ASC")
   end
-
 end
