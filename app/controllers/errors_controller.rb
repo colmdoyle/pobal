413790129d9e46b2dab92e9d486d97e0906633dc
Colm Doyle <colm@cdoyle.me>
2016-11-02 08:54:56 +0000

    Fix non-html 404s

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,9 +1,22 @@
 class ErrorsController < ApplicationController
   def not_found
-    render(:status => 404)
+    respond_to do |format|
+      format.html { render :status => 404, :formats => [:html] }
+      format.all { redirect_to "/404" }
+    end
   end
 
-  def internal_server_error
-    render(:status => 500)
+  def unacceptable
+    respond_to do |format|
+      format.html { render :status => 422 }
+      format.all { render nothing: true, status: 422 }
+    end
+  end
+
+  def internal_error
+    respond_to do |format|
+      format.html { render :layout => false, :status => 500 }
+      format.all { render nothing: true, status: 500 }
+    end
   end
 end
