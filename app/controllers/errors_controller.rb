f64e6684e3344c7983e8e7db55352ba749aa4768
Colm Doyle <colm@cdoyle.me>
2016-11-02 09:00:03 +0000

    Fix 500s page

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -13,7 +13,7 @@ class ErrorsController < ApplicationController
     end
   end
 
-  def internal_error
+  def internal_server_error
     respond_to do |format|
       format.html { render :layout => false, :status => 500 }
       format.all { render nothing: true, status: 500 }
