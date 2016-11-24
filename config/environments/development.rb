d17d34a3ac41419ba53bd7aff80a0bd7110b6587
Colm Doyle <colm@cdoyle.me>
2016-11-24 20:23:15 +0000

    some cleanup

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -40,4 +40,16 @@ Rails.application.configure do
   # config.action_view.raise_on_missing_translations = true
 
   config.assets.compress = false
+
+  config.paperclip_defaults = {
+    :storage => :s3,
+    :s3_credentials => {
+      :bucket => ENV['S3_BUCKET_NAME'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
+      :s3_region => ENV['AWS_S3_REGION']
+    },
+    :s3_protocol => 'https'
+  }
+
 end
