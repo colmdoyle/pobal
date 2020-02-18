45686ec53652f63265facacbaedeac190d72e779
Colm Doyle <colmdoyle@users.noreply.github.com>
2020-02-18 13:54:37 +0000

    Update sitemap.rb

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,5 +1,5 @@
 # Set the host name for URL creation
-SitemapGenerator::Sitemap.default_host = "http://www.whoismytd.com"
+SitemapGenerator::Sitemap.default_host = "https://www.whoismytd.com"
 
 SitemapGenerator::Sitemap.create do
   add person_index_path
