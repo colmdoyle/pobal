cceb8c27dda66f35a2ff22f2d0df128f2d6616a3
Colm Doyle <cdoyle@slack-corp.com>
2020-02-18 14:49:07 +0000

    sitemap on s3

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,5 +1,15 @@
+require 'aws-sdk'
+
 # Set the host name for URL creation
 SitemapGenerator::Sitemap.default_host = "https://www.whoismytd.com"
+SitemapGenerator::Sitemap.sitemaps_host = "https://s3-eu-west-1.amazonaws.com/whoismytd/"
+SitemapGenerator::Sitemap.public_path = 'tmp/'
+
+SitemapGenerator::Sitemap.adapter = SitemapGenerator::AwsSdkAdapter.new(ENV['S3_BUCKET_NAME'],
+  aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+  aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+  aws_region: ENV['AWS_S3_REGION']
+)
 
 SitemapGenerator::Sitemap.create do
   add person_index_path
